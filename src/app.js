04f85f1d1fb0083de2251a4b8768b1922454f95d
Thisisgerry123 <167911356+Thisisgerry123@users.noreply.github.com>
2024-06-06 02:00:13 +0000

    Did it!

--- src/app.js
+++ src/app.js
@@ -7,5 +7,30 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "my", "her", "his"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net", ".us", ".io"];
+
+  const bios = [];
+
+  for (let index = 0; index < pronoun.length; index++) {
+    for (let secondIndex = 0; secondIndex < adj.length; secondIndex++) {
+      for (let thirdIndex = 0; thirdIndex < noun.length; thirdIndex++) {
+        for (
+          let fourthIndex = 0;
+          fourthIndex < extension.length;
+          fourthIndex++
+        ) {
+          bios.push(
+            `${pronoun[index]}${adj[secondIndex]}${noun[thirdIndex]}${extension[fourthIndex]}`
+          );
+        }
+      }
+    }
+  }
+
+  for (const bio of bios) {
+    console.log(bio);
+  }
 };
